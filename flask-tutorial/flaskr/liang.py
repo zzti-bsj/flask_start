456a106ae3de773e219eeb71d61b3d928684727e
zzti-bsj <otnw_bsj@163.com>
2022-08-10 20:23:46 +0800

    update liang app

--- flask-tutorial/flaskr/liang.py
+++ flask-tutorial/flaskr/liang.py
@@ -17,15 +17,19 @@ def auth():
     if request.method == 'POST':
         return '待处理！'
     if request.method == 'GET':
-        result = {}
-        result['title'] = '离校扫码凭证'
-        result['name'] = '勾朝亮'
-        result['number'] = '2010302019'
-        result['major'] = ''
-        result['info1'] = '当天出校进校申请\n离校不离邕'
-        result['info2'] = '可以离校'
-        result['remind'] = '温馨提醒：请于离校当天23:30前返校，超过时间将不能扫码返校。'
-        result['time'] = '2022-7-31 17:30:50'
+        ## card info
+        card = {}
+        card['title'] = '离校扫码凭证'
+        card['name'] = '勾朝亮'
+        card['number'] = '2010302019'
+        card['major'] = ''
+        card['info1'] = '当天出校进校申请\n离校不离邕'
+        card['info2'] = '可以离校'
+        card['remind'] = '温馨提醒：请于离校当天23:30前返校，超过时间将不能扫码返校。'
+        card['time'] = '2022-7-31 17:30:50'
 
-        result['yuelaiyuexingle'] = '疫情防控 人人有责 仿冒凭证 依法必究'
-        return render_template('liang/auth.html', context=result)
+        card['yuelaiyuexingle'] = '疫情防控 人人有责 仿冒凭证 依法必究'
+        ## background-info
+        back_info = {}
+
+        return render_template('liang/auth.html', card=card)
